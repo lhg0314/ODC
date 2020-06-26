300ba49d02b9a221e19210d06214fb556580e7b2
GuDongyeong <ehd1722@naver.com>
2020-06-26 11:51:07 +0900

    dy 클래스 수정까지

--- src/artist/controller/ArtistClassUpdateServlet.java
+++ src/artist/controller/ArtistClassUpdateServlet.java
@@ -8,9 +8,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import artist.service.face.ArtistClassService;
 import artist.service.impl.ArtistClassServiceImpl;
+import dto.ArtistInfo;
 
 @WebServlet("/artist/class/update")
 public class ArtistClassUpdateServlet extends HttpServlet {
@@ -35,6 +37,26 @@ public class ArtistClassUpdateServlet extends HttpServlet {
 	
 		// 포워드
 		req.getRequestDispatcher("/WEB-INF/views/artist/class/artistClassView.jsp").forward(req, resp);
+	
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		// 세션값으로 로그인 아이디 가져오기
+		HttpSession session = req.getSession();
+		
+		String artId = (String) session.getAttribute("artid");
+		
+		// 해당 아이디 작가 정보 가져오기
+		ArtistInfo artInfo = artistClassService.getArtInfoByArtId(artId);
+		
+		// 클래스 신청 정보 저장하기
+		artistClassService.updateClassInfo(req, resp);
+		
+		// 리다이렉트
+		resp.sendRedirect("/artist/class/manage");
+	
 	
 	
 	
